4451a702ce012a5222f23b72ec75940a04ac889d
joha404 <johahossainm@gmail.com>
2025-04-01 09:48:36 +0600

    uploading all files

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  number: { type: Number, required: true },
+  number: { type: String, required: true },
   password: { type: String, required: true },
 });
 
